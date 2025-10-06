1ad4cc6e8fc3645d6ef0f344007cc882550583d7
Nikita Clark <nclark@junzitechsolutions.com>
2025-10-05 19:10:24 -0500

    landing 1 removed

--- src/app/landing1/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import SiteHome from "@/components/site/sections/home";
-import "@/global.css";
-import "@/styles/globals.css";
-
-export default function SitePage() {
-  return <SiteHome />;
-} 
